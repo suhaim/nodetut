a9bd7af36025f6a9ec0bb6845f48b0b674100453
suhaim <suhaim@gmail.com>
2014-08-13 10:34:00 -0300

    Update nodetut2.js

--- nodetut2.js
+++ nodetut2.js
@@ -6,14 +6,3 @@ for (var i = 0; i < process.argv.length; i++) {
     value += a;
   }
 }
-
-console.dir(value);
-
-//---------------------------------------------------
-// official solution
-  // var result = 0
-
-  //  for (var i = 2; i < process.argv.length; i++)
-  //    result += Number(process.argv[i])
-
-  //  console.log(result)
